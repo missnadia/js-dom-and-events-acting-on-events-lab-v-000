39216e7db3ea1234949eecb4e114973e66f13581
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-13 00:08:15 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,32 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  let employeeName = retrieveEmployeeInformation()
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`);
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener('click', function(event){
+    addNewElementAsLi()
+    resetInput()
+  });
+}
+
+function resetInput() {
+  document.querySelector('input').value = '';
+}
+
+function clearEmployeeListOnLinkClick() {
+  let ul = document.querySelector('ul');
+  let button = document.querySelector('a');
+  button.addEventListener('click', function(event){
+    ul.innerHTML = ''
+  });
+}
