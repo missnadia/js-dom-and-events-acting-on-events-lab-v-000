63050a426dee8d94ca11c3353a2b90e5d075b55a
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-13 00:06:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function addNewLiOnClick() {
   let submit = document.querySelector('input[type="submit"]')
   submit.addEventListener('click', function(event){
     addNewElementAsLi()
-    resetInput()
+    clearEmployeeListOnLinkClick()
   });
 }
 
